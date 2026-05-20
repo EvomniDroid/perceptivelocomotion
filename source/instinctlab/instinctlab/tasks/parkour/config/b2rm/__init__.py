58c946c9d4719e6f539c2e6aa424eb589f05cc05
“EvomniDroid” <zhanghao15@westlake.edu.cn>
2026-05-20 19:49:21 +0800

    开始调策略

---
+++ source/instinctlab/instinctlab/tasks/parkour/config/b2rm/__init__.py
@@ -0,0 +1,27 @@
+import gymnasium as gym
+
+from . import agents
+
+task_entry = "instinctlab.tasks.parkour.config.b2rm"
+
+
+gym.register(
+    id="Instinct-Parkour-Target-Amp-B2RM-v0",
+    entry_point="instinctlab.envs:InstinctRlEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{task_entry}.b2rm_parkour_cfg:B2RMParkourEnvCfg",
+        "instinct_rl_cfg_entry_point": f"{agents.__name__}.instinct_rl_amp_cfg:B2RMParkourPPORunnerCfg",
+    },
+)
+
+
+gym.register(
+    id="Instinct-Parkour-Target-Amp-B2RM-Play-v0",
+    entry_point="instinctlab.envs:InstinctRlEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{task_entry}.b2rm_parkour_cfg:B2RMParkourEnvCfg_PLAY",
+        "instinct_rl_cfg_entry_point": f"{agents.__name__}.instinct_rl_amp_cfg:B2RMParkourPPORunnerCfg",
+    },
+)
